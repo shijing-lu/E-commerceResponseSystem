256820dac9317c73966559c86f1059e989fed8b1
yaoyuhang <3129446848@qq.com>
2025-03-05 18:24:10 +0800

    init project

---
+++ src/main/java/com/shijinglu/ecommerceresponsesystem/ECommerceResponseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shijinglu.ecommerceresponsesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceResponseSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceResponseSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shijinglu/ecommerceresponsesystem/ECommerceResponseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shijinglu.ecommerceresponsesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceResponseSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
